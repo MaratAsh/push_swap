6caab5f24f9b5cf7decdb2b7b16d3e17a610b15c
Alcierra <alcierra@student.21-school.ru>
2022-03-12 18:51:15 +0300

    bonus main file refactor

--- srcs/bonus/main.c
+++ srcs/bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 17:53:51 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/11 20:31:08 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/12 16:53:28 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,13 @@ int	ft_is_sorted(t_dlist *st)
 	return (1);
 }
 
-static t_all	*ft_create(char **strs, size_t count)
-{
-	t_all	*data;
-
-	data = malloc(sizeof(t_all));
-	data->st_a = ft_convert(strs, count);
-	data->st_b = NULL;
-	return (data);
-}
-
 int	main(int argc, char **argv)
 {
 	t_all	*data;
 
 	if (argc <= 1)
 		return (0);
-	data = ft_create(argv + 1, argc - 1);
+	data = ft_create_stacks(argv + 1, argc - 1);
 	ft_process_input(data);
 	if (data->st_b == NULL && ft_is_sorted(data->st_a) == 1)
 	{
