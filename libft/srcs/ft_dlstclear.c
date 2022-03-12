91be9a11ec04ea74b4408ec6729de66f1f617568
Alcierra <alcierra@student.21-school.ru>
2022-03-12 17:09:53 +0300

    libft changed

--- libft/srcs/ft_dlstclear.c
+++ libft/srcs/ft_dlstclear.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 19:59:33 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/11 16:46:36 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/12 17:08:25 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 void	ft_dlstclear(t_dlist **dlst, void (*del)(void*))
 {
-	t_dlist	*start;
+	t_dlist	*curr;
+	t_dlist	*next;
 
 	if (*dlst)
 		return ;
-	start = *dlst;
-	while (start && start->prev)
-		start = start->prev;
-	while (start)
+	curr = *dlst;
+	while (curr && curr->prev)
+		curr = curr->prev;
+	while (curr)
 	{
-		start = start->next;
+		next = curr->next;
 		if (del)
-			del(start->prev->content);
-		free(start->prev);
+			(*del)(curr->content);
+		free(curr);
+		curr = next;
 	}
 	*dlst = NULL;
 }
