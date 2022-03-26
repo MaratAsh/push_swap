4e037bf493403680245cc753658de95c39ca72f5
Alcierra <alcierra@student.21-school.ru>
2022-03-26 17:27:29 +0300

    ft_big_sort

--- srcs/ft_big_sort.c
+++ srcs/ft_big_sort.c
@@ -6,11 +6,30 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 21:29:00 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/25 22:54:07 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/26 15:37:19 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_push_swap.h"
+
+unsigned int	ft_min_group(t_dlist *dlist)
+{
+	unsigned int	group;
+	t_data			*data;
+
+	data = ft_dlstdata(dlist);
+	if (!data)
+		return (0);
+	group = data->group;
+	while (dlist && data)
+	{
+		if (data->group < group)
+			group = data->group;
+		dlist = dlist->next;
+		data = ft_dlstdata(dlist);
+	}
+	return (group);
+}
 
 void	ft_sort__b(t_all *all)
 {
@@ -18,6 +37,7 @@ void	ft_sort__b(t_all *all)
 	t_data			*data;
 	t_data			*min_data;
 	unsigned int	middle;
+	//unsigned int	min_group;
 	size_t			i;
 
 	count = ft_dlstsize(all->st_b);
@@ -45,10 +65,11 @@ void	ft_sort__b(t_all *all)
 		min_data = ft_dlst_mindata(all->st_b);
 		middle = count / 2 + min_data->index;
 		i = 0;
+		// min_group = ft_min_group(all->st_b);
 		while (i < count)
 		{
 			data = ft_dlstdata(all->st_b);
-			if (data->index > middle)
+			if (data->index >= middle)
 			{
 				data->group++;
 				ft_putendl_fd("pa", 1);
@@ -61,7 +82,35 @@ void	ft_sort__b(t_all *all)
 			}
 			i++;
 		}
+		ft_stack_group_add(all->st_b);
 		ft_sort__b(all);
+	}
+}
+
+void	ft_push_b_greater_group(t_all *all)
+{
+	t_data			*data;
+	unsigned int	group;
+
+	data = ft_dlstdata(all->st_a);
+	group = data->group;
+	while (data && data->group == group)
+	{
+		ft_putendl_fd("pb", 1);
+		ft_operation_push_b(all);
+		data = ft_dlstdata(all->st_a);
+	}
+}
+
+void	ft_push_n_groups(t_all *all, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_push_b_greater_group(all);
+		i++;
 	}
 }
 
@@ -74,19 +123,10 @@ void	ft_big_sort(t_all *all)
 	ft_sort__b(all);
 	data = ft_dlstdata(all->st_a);
 	group = data->group;
-	while (data && data->group != 1)
+	while (data && data->index != 0)
 	{
-		if (data->group == group)
-		{
-			ft_putendl_fd("pb", 1);
-			ft_operation_push_b(all);
-		}
-		else
-		{
-			ft_sort__b(all);
-			data = ft_dlstdata(all->st_a);
-			group = data->group;
-		}
+		ft_push_n_groups(all, 1);
+		ft_sort__b(all);
 		group = data->group;
 		data = ft_dlstdata(all->st_a);
 	}
