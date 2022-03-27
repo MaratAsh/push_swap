38cca89ed4677437790288abf68c38b4a3acda51
Alcierra <alcierra@student.21-school.ru>
2022-03-27 11:22:25 +0300

    norminette main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 19:46:13 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/25 20:55:27 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/27 11:21:56 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	ft_sort_manager(data);
-	//ft_dllist_print(data->st_a, 0);
-	//ft_dllist_print(data->st_b, 0);
 	ft_stack_free(data->st_a);
 	if (data->st_b)
 		ft_stack_free(data->st_b);
