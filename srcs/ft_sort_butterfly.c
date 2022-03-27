6491bc001c291902f12ea4502abb6e2ad3748f4d
Alcierra <alcierra@student.21-school.ru>
2022-03-27 11:32:19 +0300

    butterfly sort completed

--- srcs/ft_sort_butterfly.c
+++ srcs/ft_sort_butterfly.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 09:09:06 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/27 09:33:54 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/27 11:07:58 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,32 @@ void	ft_sort_butterfly(t_all *all)
 	ft_sort_butterfly_end(all);
 }
 
+static unsigned int	ft_calc_n(t_dlist *dlist)
+{
+	size_t	count;
+
+	count = ft_dlstsize(dlist);
+	if (count <= 15)
+		return (5);
+	else if (count <= 50)
+		return (10);
+	else if (count <= 100)
+		return (15);
+	else if (count <= 100)
+		return (20);
+	else if (count <= 200)
+		return (25);
+	else if (count <= 300)
+		return (30);
+	else if (count <= 400)
+		return (35);
+	else if (count <= 500)
+		return (40);
+	else if (count <= 1000)
+		return (50);
+	return (60 + count % 1000 * 2);
+}
+
 static void	ft_sort_butterfly_start_norm(t_all *all,
 				t_data *data, unsigned int *counter_ptr)
 {
@@ -31,7 +57,7 @@ static void	ft_sort_butterfly_start_norm(t_all *all,
 		ft_putendl_fd("pb\nrb", 1);
 		(*counter_ptr)++;
 	}
-	else if (data->index <= *counter_ptr + 1)
+	else if (data->index <= *counter_ptr + ft_calc_n(all->st_b))
 	{
 		ft_operation_push_b(all);
 		ft_putendl_fd("pb", 1);
@@ -59,25 +85,6 @@ static void	ft_sort_butterfly_start(t_all *all)
 
 static void	ft_sort_butterfly_end(t_all *all)
 {
-	t_dlist			*tail;
-	t_data			*tail_data;
-	t_data			*data;
-
 	while (all->st_b)
-	{
-		data = ft_dlstdata(all->st_b);
-		tail = ft_dlstlast(all->st_b);
-		tail_data = ft_dlstdata(tail);
-		if (data->index >= tail_data->index)
-		{
-			ft_operation_push_a(all);
-			ft_putendl_fd("pa", 1);
-		}
-		else
-		{
-			ft_operation_revrotate_b(all);
-			ft_operation_push_a(all);
-			ft_putendl_fd("rrb\npa", 1);
-		}
-	}
+		ft_push_a_max_b(all);
 }
