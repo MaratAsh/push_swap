ff97985b2ef8f4ee5bb940d7e3ef88ff01628cda
Alcierra <alcierra@student.21-school.ru>
2022-03-27 10:44:12 +0300

    bug with 1 length list  was fixed

--- srcs/ft_dlstdata_max.c
+++ srcs/ft_dlstdata_max.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 09:49:48 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/27 09:55:03 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/27 10:43:18 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_dlist	*ft_dlstdata_max(t_dlist *dlist)
 	t_dlist			*max_index_dlist;
 
 	max_index = 0;
-	max_index_dlist = NULL;
+	max_index_dlist = dlist;
 	while (dlist)
 	{
 		data = ft_dlstdata(dlist);
@@ -40,7 +40,7 @@ unsigned int	ft_dlstdata_max_index(t_dlist *dlist)
 	t_dlist			*max_index_dlist;
 
 	max_index = 0;
-	max_index_dlist = NULL;
+	max_index_dlist = dlist;
 	while (dlist)
 	{
 		data = ft_dlstdata(dlist);
